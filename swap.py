1b682d66e27220dd296d61bb87818e8bbde41b82
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-08 03:07:23 +0600

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,42 @@
+#Ex:1
+a = 5
+b = 6
+temp = a
+a = b
+b = temp
+
+print(a)
+print(b)
+
+#Ex:2
+a = 5
+b = 6
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a)
+print(b)
+
+
+#Ex:3
+a = 5
+b = 6
+
+a = a ^ b
+b = a ^ b
+a = a ^ b
+
+print(a)
+print(b)
+
+#Ex:4
+a = 5
+b = 6
+
+a,b = b,a
+
+
+print(a)
+print(b)
